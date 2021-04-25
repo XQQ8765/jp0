4d9aa7585d48c3ae4f5fca40441e0c34c44f383d
Rabbit Xiao (rxiao) <Rabbit.Xiao@quest.com>
2021-04-25 16:50:14 +0800

    Improve "MyBinarySearch" version 2.

--- practise/src/main/java/org/rabbit/jp0/practise/BinarySearch/MyBinarySearch.java
+++ practise/src/main/java/org/rabbit/jp0/practise/BinarySearch/MyBinarySearch.java
@@ -16,38 +16,26 @@ public class MyBinarySearch {
     }
 
     private int binarySearch(int[] arr, int start, int end, int k) {
+        //System.out.println("start:" + start + ", end:" + end +", k:" + k);
         if (start > end) {
             return -1;
         }
-        if (start == end) {
-            if (arr[start] == k) {
-                return end;
-            } else {
-                return -1;
-            }
-        }
 
-        int mid = start + (end - start) / 2;;
-        System.out.println("start:"+start + ", end=" + end + ", k=" + k + ", mid="+mid);
-        if (arr[mid] == k) {
+        int mid = start + (end - start) / 2;
+        if (k == arr[mid]) {
             return mid;
+        } else if (k < arr[mid]) {
+            return binarySearch(arr, 0, mid-1, k);
+        } else {
+            return binarySearch(arr, mid+1, end, k);
         }
-
-        int index = binarySearch(arr, 0, mid-1, k);
-        if (index != -1) {
-            return index;
-        }
-        index = binarySearch(arr, mid+1, end, k);
-        if (index != -1) {
-            return index;
-        }
-        return -1;
     }
 
     public static void main(String[] args) {
         MyBinarySearch mbs = new MyBinarySearch();
         int[] arr = {2, 4, 6, 8, 10, 12, 14, 16};
         System.out.println("Key 14's position: "+mbs.binarySearch(arr, 14));
+        System.out.println("Key 7's position: "+mbs.binarySearch(arr, 7));
         int[] arr1 = {6,34,78,123,432,900};
         System.out.println("Key 432's position: "+mbs.binarySearch(arr1, 432));
     }
