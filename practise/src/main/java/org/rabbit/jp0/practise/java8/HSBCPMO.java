1a0ec78146623edd36d7146b6d11a512cc937410
Rabbit Xiao (rxiao) <Rabbit.Xiao@quest.com>
2021-04-30 20:45:32 +0800

    Add “StreamAPIExcersize”

---
+++ practise/src/main/java/org/rabbit/jp0/practise/java8/HSBCPMO.java
@@ -0,0 +1,40 @@
+package org.rabbit.jp0.practise.java8;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Stream;
+
+/**
+ * private static String[][] myStringMatrix = {
+ * {"10-11", "8-4", "3-7"},
+ * {"11-10", "3-8", "7-6"},
+ * {"5-2", "4-3", "4-4"}
+ * };
+ * <p>
+ * // TODO: Please implement a function to sort the
+ * // input matrix and return it as 1D array.
+ * // The sort order is:
+ * // sort by the first number asc, if the firt numbers equals,
+ * // then sort by the second number desc.
+ * // Example output:3-8, 3-7, 4-4, 4-3,5-2, 7-6, 8-4, 10-11
+ */
+public class HSBCPMO {
+    private static String[][] myStringMatrix = {
+            {"10-11", "8-4", "3-7"},
+            {"11-10", "3-8", "7-6"},
+            {"5-2", "4-3", "4-4"}
+    };
+
+    public static void main(String[] args) {
+        Arrays.stream(myStringMatrix).flatMap(Stream::of).sorted(
+                (s1, s2) -> {
+                    int[] s1Arr = Stream.of(s1.split("-")).mapToInt(s -> Integer.parseInt(s)).toArray();
+                    int[] s2Arr = Stream.of(s2.split("-")).mapToInt(s -> Integer.parseInt(s)).toArray();
+                    if (s1Arr[0] == s2Arr[0]) {
+                        return -1 * (s1Arr[1] - s2Arr[1]);
+                    }
+                    return s1Arr[0] - s2Arr[0];
+                }
+        ).forEach(System.out::println);
+    }
+}
---
+++ practise/src/main/java/org/rabbit/jp0/practise/java8/StreamAPIExcersize.java
@@ -0,0 +1,151 @@
+package org.rabbit.jp0.practise.java8;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * JAVA8新特性之StreamAPI简单练习 https://blog.csdn.net/K_520_W/article/details/83032889
+ */
+public class StreamAPIExcersize {
+    public static void main(String[] args) {
+        test1();
+        testEmployees();
+        testTransaction();
+    }
+    /**
+    给定一个数字列表，如何返回一个由每个数的平方构成的数字列表呢？
+        例如：给定[1,2,3,4,5]，返回[1,4,9,16,25]
+     */
+    public static void test1() {
+        int[] arrays = new int[]{1,2,3,4,5};
+        Arrays.stream(arrays).map(i-> i*i).forEach(System.out::println);
+    }
+
+    /**
+     * 怎样使用map和reduce方法数一数流中有多少个Employee呢？
+     */
+    public static void testEmployees() {
+        List<Employee> employees = Arrays.asList(
+                new Employee("ABC", 10),
+                new Employee("BCD", 50),
+                new Employee("XXX", 100),
+                new Employee("MMM", 30),
+                new Employee("MMM", 30),
+                new Employee("BCD", 50)
+        );
+
+       int count = employees.stream().map(e -> 1).reduce(Integer::sum).get();
+       System.out.println("Employee count:" + count);
+    }
+
+    public static void testTransaction() {
+        List<Transaction> transactions0 = getTransactionList();
+        //找出2020年发生的所有交易， 并按交易额排序（从低到高）
+        System.out.println("---------------020年发生的所有交易， 并按交易额排序（从低到高）:");
+        transactions0.stream().filter(t -> t.year == 2020).sorted((t1, t2) -> t1.value - t2.value).forEach(System.out::println);
+
+        transactions0 = getTransactionList();
+        //交易员都在哪些不同的城市工作过？
+        System.out.println("---------------交易员都在哪些不同的城市工作过？:");
+        transactions0.stream().map(t -> t.trade.city).distinct().forEach(System.out::println);
+
+        //查找所有来自Beijing的交易员，并按姓名排序
+        System.out.println("---------------查找所有来自Beijing的交易员，并按姓名排序:");
+        transactions0.stream().map(t -> t.trade).filter(t -> t.city == "Beijing")
+                .map(t -> t.name)
+                .distinct().sorted().forEach(System.out::println);
+
+        //返回所有交易员的姓名字符串，按字母顺序排序
+        System.out.println("---------------返回所有交易员的姓名字符串，按字母顺序排序:");
+        String connectedNameChars = transactions0.stream().map(t -> t.trade.name)
+                .flatMap(name -> name.chars().mapToObj(c -> (char) c))
+                .sorted((c1, c2) -> c1.compareTo(c2))
+                .map(c -> c.toString())
+                .collect(Collectors.joining());
+        System.out.println(connectedNameChars);
+
+    }
+
+    private static List<Transaction>  getTransactionList() {
+        Trade trade1 = new Trade("trade-1", "Beijing");
+        Trade trade2 = new Trade("trade-2", "Zhuhai");
+        Trade trade3 = new Trade("trade-3", "Beijing");
+        Trade trade4 = new Trade("trade-4", "GuangZhou");
+
+        List<Transaction> transactions = Arrays.asList(
+                new Transaction(trade1, 2020, 100),
+                new Transaction(trade2, 2021, 200),
+                new Transaction(trade3, 2019, 100),
+                new Transaction(trade3, 2020, 300),
+                new Transaction(trade4, 2020, 200),
+                new Transaction(trade1, 2021, 600));
+
+        return transactions;
+    }
+
+    static class Employee {
+        String name;
+        int salary;
+
+        public Employee(String name, int salary) {
+            this.name = name;
+            this.salary = salary;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getSalary() {
+            return salary;
+        }
+
+        @Override
+        public String toString() {
+            return "Employee{" +
+                    "name='" + name + '\'' +
+                    ", salary=" + salary +
+                    '}';
+        }
+    }
+
+    static class Trade {
+        String name;
+        String city;
+
+        public Trade(String name, String city) {
+            this.name = name;
+            this.city = city;
+        }
+
+        @Override
+        public String toString() {
+            return "Trade{" +
+                    "name='" + name + '\'' +
+                    ", city='" + city + '\'' +
+                    '}';
+        }
+    }
+    static class Transaction {
+        Trade trade;
+        int year;
+        int value;
+
+        public Transaction(Trade trade, int year, int value) {
+            this.trade = trade;
+            this.year = year;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Transaction{" +
+                    "trade=" + trade +
+                    ", year=" + year +
+                    ", value=" + value +
+                    '}';
+        }
+    }
+}
